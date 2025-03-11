4a9e90149be3a3d5ea65a794eec85dafbf760ef7
Alexa Dandrea <alexadandrea818@gmail.com>
2025-03-11 13:57:41 -0400

    update database

--- mc_basic_code/mc_database.sql
+++ mc_basic_code/mc_database.sql
@@ -11,58 +11,65 @@ CREATE TABLE users (
 
 CREATE TABLE workouts (
    workoutID INT PRIMARY KEY AUTO_INCREMENT,
-   userID INT FOREIGN KEY REFERENCES users(user_id),
+   userID INT,
    workoutType VARCHAR(100) NOT NULL,
    duration INT NOT NULL, -- in minutes
    caloriesBurned INT NOT NULL,
    startTime DATETIME NOT NULL,
    endTime DATETIME NOT NULL
+   FOREIGN KEY (userID) REFERENCES users(user_id),
 );
 
 CREATE TABLE exercises (
    exerciseID INT PRIMARY KEY AUTO_INCREMENT,
-   workoutID INT FOREIGN KEY REFERENCES workouts(workoutID),
+   workoutID INT,
    exerciseName VARCHAR(100) NOT NULL,
    sets INT NOT NULL,
    reps INT NOT NULL,
-   weight DECIMAL(5, 2) NULL -- in LBS
+   weight DECIMAL(5, 2) NULL, -- in LBS
+   FOREIGN KEY (workoutID) REFERENCES workouts(workoutID)
 );
 
 CREATE TABLE nutrition (
    nutritionID INT PRIMARY KEY AUTO_INCREMENT,
-   userID INT FOREIGN KEY REFERENCES users(user_id),
+   userID INT,
    foodItem VARCHAR(255) NOT NULL,
    calories INT NOT NULL,
    protein DECIMAL(5, 2) NULL, -- in grams
    fats DECIMAL(5, 2) NULL, -- in grams
-   mealTime DATETIME NOT NULL
+   mealTime DATETIME NOT NULL,
+   FOREIGN KEY (userID) REFERENCES users(user_id)
 );
 
 CREATE TABLE progress (
    progressID INT PRIMARY KEY AUTO_INCREMENT,
-   userID INT FOREIGN KEY REFERENCES users(user_id),
+   userID INT,
    weight DECIMAL(5, 2) NULL, -- in LBS
    bodyFat DECIMAL(5, 2) NULL, -- in percentage
    muscleMass DECIMAL(5, 2) NULL, -- in kg
-   recordedAT DATETIME NOT NULL
+   recordedAT DATETIME NOT NULL,
+   FOREIGN KEY (userID) REFERENCES users(user_id)
 );
 
 CREATE TABLE subscription (
    subscriptionID INT PRIMARY KEY AUTO_INCREMENT,
-   userID INT FOREIGN KEY REFERENCES users(user_id),
+   userID INT,
    planName VARCHAR(100) NOT NULL,
    price DECIMAL(6,2) NOT NULL,
    startDate DATETIME NOT NULL,
-   endDate DATETIME NULL
+   endDate DATETIME NULL,
+   FOREIGN KEY (userID) REFERENCES users(user_id),
 );
 
 CREATE TABLE payments (
    paymentID INT PRIMARY KEY AUTO_INCREMENT,
-   userID INT FOREIGN KEY REFERENCES users(user_id),
-   subscriptionID INT FOREIGN KEY REFERENCES subscription(subscriptionID),
+   userID INT,
+   subscriptionID INT,
    amountPaid DECIMAL(6,2) NOT NULL,
    paymentDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   paymentMethod VARCHAR(50) NOT NULL
+   paymentMethod VARCHAR(50) NOT NULL,
+   FOREIGN KEY (userID) REFERENCES users(user_id),
+   FOREIGN KEY (subscriptionID) REFERENCES subscription(subscriptionID)
 );
 
 CREATE TABLE admin (

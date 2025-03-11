556e16bdb07243c3f7289f007b912a652d7d8508
Alexa Dandrea <alexadandrea818@gmail.com>
2025-03-11 13:17:15 -0400

    database sql code

---
+++ mc_basic_code/mc_database.sql
@@ -0,0 +1,73 @@
+CREATE TABLE users (
+   user_id INT PRIMARY KEY IDENTITY(1,1),
+   firstName VARCHAR(255) NOT NULL,
+   lastName VARCHAR(255) NOT NULL,
+   email VARCHAR(255) NOT NULL,
+   passwordHash VARCHAR(255) NOT NULL,
+   dateOfBirth DATE NULL,
+   gender ENUM('M', 'F', 'Other') NULL,
+   createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE workouts (
+   workoutID INT PRIMARY KEY IDENTITY(1,1),
+   userID INT FOREIGN KEY REFERENCES users(user_id),
+   workoutType VARCHAR(100) NOT NULL,
+   duration INT NOT NULL, -- in minutes
+   caloriesBurned INT NOT NULL,
+   startTime DATETIME NOT NULL,
+   endTime DATETIME NOT NULL
+);
+
+CREATE TABLE exercises (
+   exerciseID INT PRIMARY KEY IDENTITY(1,1),
+   workoutID INT FOREIGN KEY REFERENCES workouts(workoutID),
+   exerciseName VARCHAR(100) NOT NULL,
+   sets INT NOT NULL,
+   reps INT NOT NULL,
+   weight DECIMAL(5, 2) NULL -- in LBS
+);
+
+CREATE TABLE nutrition (
+   nutritionID INT PRIMARY KEY IDENTITY(1,1),
+   userID INT FOREIGN KEY REFERENCES users(user_id),
+   foodItem VARCHAR(255) NOT NULL,
+   calories INT NOT NULL,
+   protein DECIMAL(5, 2) NULL, -- in grams
+   fats DECIMAL(5, 2) NULL, -- in grams
+   mealTime DATETIME NOT NULL
+);
+
+CREATE TABLE progress (
+   progressID INT PRIMARY KEY IDENTITY(1,1),
+   userID INT FOREIGN KEY REFERENCES users(user_id),
+   weight DECIMAL(5, 2) NULL, -- in LBS
+   bodyFat DECIMAL(5, 2) NULL, -- in percentage
+   muscleMass DECIMAL(5, 2) NULL, -- in kg
+   recordedAT DATETIME NOT NULL
+);
+
+CREATE TABLE subscription (
+   subscriptionID INT PRIMARY KEY IDENTITY(1,1),
+   userID INT FOREIGN KEY REFERENCES users(user_id),
+   planName VARCHAR(100) NOT NULL,
+   price DECIMAL(6,2) NOT NULL,
+   startDate DATETIME NOT NULL,
+   endDate DATETIME NULL
+);
+
+CREATE TABLE payments (
+   paymentID INT PRIMARY KEY IDENTITY(1,1),
+   userID INT FOREIGN KEY REFERENCES users(user_id),
+   subscriptionID INT FOREIGN KEY REFERENCES subscription(subscriptionID),
+   amountPaid DECIMAL(6,2) NOT NULL,
+   paymentDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   paymentMethod VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE admin (
+   adminID INT PRIMARY KEY IDENTITY(1,1),
+   username VARCHAR(100) NOT NULL,
+   passwordHash VARCHAR(255) NOT NULL,
+   role ENUM('Super Admin', 'Support', 'Manager') NOT NULL,
+);
